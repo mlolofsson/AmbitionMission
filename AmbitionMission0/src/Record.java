d9f56762736d8442ea737e1f2a941b400d766ea9
Michelle <olofssonmichelle@gmail.com>
2018-05-11 14:00:23 -0400

    Added record class

---
+++ AmbitionMission0/src/Record.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Record {
+
+		private ArrayList<Goal> GList;
+		private ArrayList<Habit> HList;
+		private ArrayList<String> MList;
+
+		public Record(){
+			GList = new ArrayList<Goal>();
+			HList = new ArrayList<Habit>();
+			MList = new ArrayList<String>();
+
+		}
+
+		public void addGoal(User e){
+
+		}
+
+		public void storeGoal(){
+			
+		}
+
+		public void storeMood(){
+
+		}
+
+		public void storeHabit(){
+
+		}
+
+	}
